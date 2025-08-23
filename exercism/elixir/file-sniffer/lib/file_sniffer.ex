281ba41d14ce239db7f161792c3fc40cdf016b09
Saull Brandão <saullrb@proton.me>
2025-08-23 14:38:07 -0300

    exercism file-sniffer

---
+++ exercism/elixir/file-sniffer/lib/file_sniffer.ex
@@ -0,0 +1,43 @@
+defmodule FileSniffer do
+  @extension_to_media_type %{
+    "exe" => "application/octet-stream",
+    "bmp" => "image/bmp",
+    "png" => "image/png",
+    "jpg" => "image/jpg",
+    "gif" => "image/gif"
+  }
+
+  def type_from_extension(extension), do: @extension_to_media_type[extension]
+
+  def type_from_binary(file_binary) do
+    case file_binary do
+      <<0x7F, 0x45, 0x4C, 0x46, _::binary>> ->
+        type_from_extension("exe")
+
+      <<0x42, 0x4D, _::binary>> ->
+        type_from_extension("bmp")
+
+      <<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, _::binary>> ->
+        type_from_extension("png")
+
+      <<0xFF, 0xD8, 0xFF, _::binary>> ->
+        type_from_extension("jpg")
+
+      <<0x47, 0x49, 0x46, _::binary>> ->
+        type_from_extension("gif")
+
+      _ ->
+        nil
+    end
+  end
+
+  def verify(file_binary, extension) do
+    case {type_from_extension(extension), type_from_binary(file_binary)} do
+      {type, type} when not is_nil(type) ->
+        {:ok, type}
+
+      _ ->
+        {:error, "Warning, file format and file extension do not match."}
+    end
+  end
+end
